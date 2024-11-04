b9a7f2bc17f8250edaab3353c26c48244f8d1074
Khanerf <erfankheyrkhah85@gmail.com>
2024-11-04 13:33:22 +0330

    Kheirkhah

---
+++ session1.cs
@@ -0,0 +1,24 @@
+Console.Write("Enter number of students: ");
+            int numberOfStudents = int.Parse(Console.ReadLine());
+
+            string[] names = new string[numberOfStudents];
+            double[] scores = new double[numberOfStudents];
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                Console.Write($"Enter students name {i + 1} : ");
+                names[i] = Console.ReadLine();
+
+                Console.Write($"Enter students score {i + 1} : ");
+                scores[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("nResult:");
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                int roundedScore = (int)Math.Round(scores[i]);
+                string status = roundedScore > 10 ? "Pass" : "Fail";
+
+                Console.WriteLine($"Name: {names[i]}, Score: {roundedScore}, Status: {status}");
+            }
+            Console.ReadKey();
